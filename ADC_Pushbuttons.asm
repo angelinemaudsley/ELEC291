f0266a45eda4a2c9407e87e9f7bae08dac50591b
angelinemaudsley <angelinemaudsleyy@gmail.com>
2025-02-01 16:12:34 -0800

    Update ADC_Pushbuttons.asm

--- ADC_Pushbuttons.asm
+++ ADC_Pushbuttons.asm
@@ -22,14 +22,19 @@ $LIST
 CLK               EQU 16600000 ; Microcontroller system frequency in Hz
 BAUD              EQU 115200 ; Baud rate of UART in bps
 TIMER1_RELOAD     EQU (0x100-(CLK/(16*BAUD)))
-TIMER0_RELOAD_1MS EQU (0x10000-(CLK/1000))
+TIMER0_RATE   EQU 4096     ; 2048Hz squarewave (peak amplitude of CEM-1203 speaker)
+TIMER0_RELOAD EQU ((65536-(CLK/TIMER0_RATE)))
+TIMER2_RATE   EQU 1000     ; 1000Hz, for a timer tick of 1ms
+TIMER2_RELOAD EQU ((65536-(CLK/TIMER2_RATE)))
+
+START_BUTTON  equ 
 
 ORG 0x0000
 	ljmp main
 
 ;              1234567890123456    <- This helps determine the location of the counter
-title:     db 'ADC PUSH BUTTONS', 0
-blank:     db '                ', 0
+title:     db 'Soak: xxs xxxC', 0
+blank:     db 'Reflow: xxs xxxC', 0
 
 cseg
 ; These 'equ' must match the hardware wiring
@@ -44,6 +49,13 @@ $NOLIST
 $include(LCD_4bit.inc) ; A library of LCD related functions and utility macros
 $LIST
 
+DSEG at 0x30
+STATE: ds 1
+Soak_time: ds 1
+Soak_temp: ds 1
+Reflow_time: ds 1
+Reflow_temp: ds 1
+
 BSEG
 ; These eight bit variables store the value of the pushbuttons after calling 'ADC_to_PB' below
 PB0: dbit 1
@@ -51,9 +63,7 @@ PB1: dbit 1
 PB2: dbit 1
 PB3: dbit 1
 PB4: dbit 1
-PB5: dbit 1
-PB6: dbit 1
-PB7: dbit 1
+Decrement: dbit 1
 
 CSEG
 Init_All:
@@ -93,20 +103,84 @@ Init_All:
 	
 	ret
 	
-wait_1ms:
-	clr	TR0 ; Stop timer 0
-	clr	TF0 ; Clear overflow flag
-	mov	TH0, #high(TIMER0_RELOAD_1MS)
-	mov	TL0,#low(TIMER0_RELOAD_1MS)
-	setb TR0
-	jnb	TF0, $ ; Wait for overflow
+Timer0_Init:
+	orl CKCON, #0b00001000 ; Input for timer 0 is sysclk/1
+	mov a, TMOD
+	anl a, #0xf0 ; 11110000 Clear the bits for timer 0
+	orl a, #0x01 ; 00000001 Configure timer 0 as 16-timer
+	mov TMOD, a
+	mov TH0, #high(TIMER0_RELOAD)
+	mov TL0, #low(TIMER0_RELOAD)
+	; Enable the timer and interrupts
+    setb ET0  ; Enable timer 0 interrupt
+    setb TR0  ; Start timer 0
 	ret
 
-; Wait the number of miliseconds in R2
-waitms:
-	lcall wait_1ms
-	djnz R2, waitms
+;---------------------------------;
+; ISR for timer 0.  Set to execute;
+; every 1/4096Hz to generate a    ;
+; 2048 Hz wave at pin SOUND_OUT   ;
+;---------------------------------;
+Timer0_ISR:
+	;clr TF0  ; According to the data sheet this is done for us already.
+	; Timer 0 doesn't have 16-bit auto-reload, so
+	clr TR0
+	mov TH0, #high(TIMER0_RELOAD)
+	mov TL0, #low(TIMER0_RELOAD)
+	setb TR0
+	reti
+
+;---------------------------------;
+; Routine to initialize the ISR   ;
+; for timer 2                     ;
+;---------------------------------;
+Timer2_Init:
+	mov T2CON, #0 ; Stop timer/counter.  Autoreload mode.
+	mov TH2, #high(TIMER2_RELOAD)
+	mov TL2, #low(TIMER2_RELOAD)
+	; Set the reload value
+	orl T2MOD, #0x80 ; Enable timer 2 autoreload
+	mov RCMP2H, #high(TIMER2_RELOAD)
+	mov RCMP2L, #low(TIMER2_RELOAD)
+	; Init One millisecond interrupt counter.  It is a 16-bit variable made with two 8-bit parts
+	clr a
+	mov Count1ms+0, a
+	mov Count1ms+1, a
+	; Enable the timer and interrupts
+	orl EIE, #0x80 ; Enable timer 2 interrupt ET2=1
+    setb TR2  ; Enable timer 2
 	ret
+
+;---------------------------------;
+; ISR for timer 2                 ;
+;---------------------------------;
+Timer2_ISR:
+	clr TF2  ; Timer 2 doesn't clear TF2 automatically. Do it in the ISR.  It is bit addressable.
+	cpl P0.4 ; To check the interrupt rate with oscilloscope. It must be precisely a 1 ms pulse.
+	
+	; The two registers used in the ISR must be saved in the stack
+	push acc
+	push psw
+	
+	; Increment the 16-bit one mili second counter
+	inc Count1ms+0    ; Increment the low 8-bits first
+	mov a, Count1ms+0 ; If the low 8-bits overflow, then increment high 8-bits
+	jnz Inc_Done
+	inc Count1ms+1
+
+Inc_Done:
+	; Check if second has passed
+	mov a, Count1ms+0
+	cjne a, #low(1000), Timer2_ISR_done ; Warning: this instruction changes the carry flag!
+	mov a, Count1ms+1
+	cjne a, #high(1000), Timer2_ISR_done
+	
+	; 1000 milliseconds have passed.  Set a flag so the main program knows
+	setb seconds_flag ; Let the main program know second had passed
+	; Reset to zero the milli-seconds counter, it is a 16-bit variable
+	clr a
+	mov Count1ms+0, a
+	mov Count1ms+1, a
 
 ADC_to_PB:
 	anl ADCCON0, #0xF0
@@ -116,41 +190,12 @@ ADC_to_PB:
 	setb ADCS   ; ADC start trigger signal
     jnb ADCF, $ ; Wait for conversion complete
 
-	setb PB7
-	setb PB6
-	setb PB5
 	setb PB4
 	setb PB3
 	setb PB2
 	setb PB1
 	setb PB0
 	
-	; Check PB7
-ADC_to_PB_L7:
-	clr c
-	mov a, ADCRH
-	subb a, #0xf0
-	jc ADC_to_PB_L6
-	clr PB7
-	ret
-
-	; Check PB6
-ADC_to_PB_L6:
-	clr c
-	mov a, ADCRH
-	subb a, #0xd0
-	jc ADC_to_PB_L5
-	clr PB6
-	ret
-
-	; Check PB5
-ADC_to_PB_L5:
-	clr c
-	mov a, ADCRH
-	subb a, #0xb0
-	jc ADC_to_PB_L4
-	clr PB5
-	ret
 
 	; Check PB4
 ADC_to_PB_L4:
@@ -158,7 +203,18 @@ ADC_to_PB_L4:
 	mov a, ADCRH
 	subb a, #0x90
 	jc ADC_to_PB_L3
-	clr PB4
+	jb decrement, Soak_time_decrement
+	mov a, Soak_time
+	add a, #0x01
+	da a
+	mov Soak_time, a
+	ret
+
+Soak_time_decrement: 
+	mov a, Soak_time
+	add a, #0x99
+	da a
+	mov Soak_time, a
 	ret
 
 	; Check PB3
@@ -167,7 +223,24 @@ ADC_to_PB_L3:
 	mov a, ADCRH
 	subb a, #0x70
 	jc ADC_to_PB_L2
-	clr PB3
+	jb decrement, Soak_temp_decrement
+	mov a, Soak_temp
+	add a, #0x01
+	da a
+	mov Soak_temp, a
+	cjne a, #0x250, ADC_to_PB_L2
+	mov a, #0x00
+	mov Soak_temp, a
+	ret
+
+Soak_temp_decrement: 
+	mov a, Soak_temp
+	add a, #0x99
+	da a
+	mov Soak_temp, a
+	cjne a, #0x250, ADC_to_PB_L2
+	mov a, #0x00
+	mov Soak_temp, a
 	ret
 
 	; Check PB2
@@ -176,7 +249,18 @@ ADC_to_PB_L2:
 	mov a, ADCRH
 	subb a, #0x50
 	jc ADC_to_PB_L1
-	clr PB2
+	jb decrement, Reflow_time_decrement
+	mov a, Reflow_time
+	add a, #0x01
+	da a
+	mov Reflow_time, a
+	ret
+
+Reflow_time_decrement: 
+	mov a, Reflow_time
+	add a, #0x99
+	da a
+	mov Reflow_time, a
 	ret
 
 	; Check PB1
@@ -185,7 +269,24 @@ ADC_to_PB_L1:
 	mov a, ADCRH
 	subb a, #0x30
 	jc ADC_to_PB_L0
-	clr PB1
+	jb decrement, Reflow_temp_decrement
+	mov a, Reflow_temp
+	add a, #0x01
+	da a
+	mov Reflow_temp, a
+	cjne a, #0x250, ADC_to_PB_L0
+	mov a, #0x00
+	mov Reflow_temp, a
+	ret
+
+Reflow_temp_decrement: 
+	mov a, Reflow_temp
+	add a, #0x99
+	da a
+	mov Reflow_temp, a
+	cjne a, #0x250, ADC_to_PB_L0
+	mov a, #0x00
+	mov Reflow_temp, a
 	ret
 
 	; Check PB0
@@ -194,68 +295,59 @@ ADC_to_PB_L0:
 	mov a, ADCRH
 	subb a, #0x10
 	jc ADC_to_PB_Done
-	clr PB0
+	clp decrement
 	ret
 	
 ADC_to_PB_Done:
 	; No pusbutton pressed	
 	ret
 
-Display_PushButtons_ADC:
-	Set_Cursor(2, 1)
-	mov a, #'0'
-	mov c, PB7
-	addc a, #0
-    lcall ?WriteData	
-	mov a, #'0'
-	mov c, PB6
-	addc a, #0
-    lcall ?WriteData	
-	mov a, #'0'
-	mov c, PB5
-	addc a, #0
-    lcall ?WriteData	
-	mov a, #'0'
-	mov c, PB4
-	addc a, #0
-    lcall ?WriteData	
-	mov a, #'0'
-	mov c, PB3
-	addc a, #0
-    lcall ?WriteData	
-	mov a, #'0'
-	mov c, PB2
-	addc a, #0
-    lcall ?WriteData	
-	mov a, #'0'
-	mov c, PB1
-	addc a, #0
-    lcall ?WriteData	
-	mov a, #'0'
-	mov c, PB0
-	addc a, #0
-    lcall ?WriteData	
+Check_start:
+	jb START_BUTTON, skip  ; if the 'CLEAR' button is not pressed skip
+	Wait_Milli_Seconds(#50)	; Debounce delay.  This macro is also in 'LCD_4bit.inc'
+	jb  START_BUTTON, skip  ; if the 'CLEAR' button is not pressed skip
+	jnb START_BUTTON, $		; Wait for button release.  The '$' means: jump to same instruction.
+	mov STATE, #0x01
+
+skip:
 	ret
 	
 main:
 	mov sp, #0x7f
 	lcall Init_All
-    lcall LCD_4BIT
+	lcall LCD_4BIT
     
     ; initial messages in LCD
 	Set_Cursor(1, 1)
     Send_Constant_String(#Title)
 	Set_Cursor(2, 1)
     Send_Constant_String(#blank)
+    mov STATE, #0x00
+    mov Soak_time, #0x00
+    mov Soak_temp, #0x00
+    mov Reflow_time, #0x00
+    mov Reflow_temp, #0x00
+    clr Decrement
 	
 Forever:
+
+	mov a, STATE
+
+	state_0: 
+	cnje a, #0, state_1
 	lcall ADC_to_PB
-	lcall Display_PushButtons_ADC
-	
-	; Wait 50 ms between readings
-	mov R2, #50
-	lcall waitms
-	
+	Set_Cursor(1,7) 
+	Display_BCD(Soak_time)
+	Set_Cursor((1,11)
+	Display_BCD(Soak_temp)
+	Set_Cursor(2,9)
+	Display_BCD(Reflow_time)
+	Set_Cursor(2,13)
+	Display_BCD(Reflow_temp)
+	lcall Check_start
+	ljmp state_0
+
+
 	ljmp Forever
 	
 END
