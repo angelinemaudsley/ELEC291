341503905950133995260413a9a8a51c03c6a8aa
angelinemaudsley <angelinemaudsleyy@gmail.com>
2025-02-01 18:12:06 -0800

    Update Reflow_Controller.asm

--- Reflow_Controller.asm
+++ Reflow_Controller.asm
@@ -195,7 +195,7 @@ check_stemp:
 	add a, #0x01
 	da a
 	mov Soak_temp, a
-	cjne a, #0x250, check_rtime
+	cjne a, #250, check_rtime
 	mov a, #0x00
 	mov Soak_temp, a
 	ljmp check_rtime
@@ -205,7 +205,7 @@ Soak_temp_decrement:
 	add a, #0x99
 	da a
 	mov Soak_temp, a
-	cjne a, #0x250, check_rtime
+	cjne a, #250, check_rtime
 	mov a, #0x00
 	mov Soak_temp, a
 	ljmp check_rtime
@@ -233,7 +233,7 @@ check_rtemp:
 	add a, #0x01
 	da a
 	mov Reflow_temp, a
-	cjne a, #0x250, skipp
+	cjne a, #250, skipp
 	mov a, #0x00
 	mov Reflow_temp, a
 	ljmp skipp
@@ -243,7 +243,7 @@ Reflow_temp_decrement:
 	add a, #0x99
 	da a
 	mov Reflow_temp, a
-	cjne a, #0x250, skipp
+	cjne a, #250, skipp
 	mov a, #0x00
 	mov Reflow_temp, a
 	ljmp skipp
