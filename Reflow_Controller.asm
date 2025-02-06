00bb945970f8f141e6f51d792aed74def1a15321
angelinemaudsley <angelinemaudsleyy@gmail.com>
2025-02-06 11:49:23 -0800

    Update Reflow_Controller.asm

--- Reflow_Controller.asm
+++ Reflow_Controller.asm
@@ -617,7 +617,7 @@ main:
      ; initial messages in LCD
     mov STATE, #0x00
     mov Soak_time, #0x00
-    mov Soak_temp, #0x40
+    mov Soak_temp, #0x00
     mov soak_temp_hund, #0x00
     mov Reflow_time, #0x00
     mov Reflow_temp, #0x00
